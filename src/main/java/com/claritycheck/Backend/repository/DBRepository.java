c8d3cf609d11733480851b6553cb0166e4d26d20
arancksj22 <arancksj@gmail.com>
2025-09-29 21:44:28 +0530

    dbrepo

--- src/main/java/com/claritycheck/Backend/repository/DBRepository.java
+++ src/main/java/com/claritycheck/Backend/repository/DBRepository.java
@@ -4,6 +4,8 @@ import com.claritycheck.Backend.model.AgentHistory;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 @Repository
-public interface DBRepository extends JpaRepository<AgentHistory, id>{
+public interface DBRepository extends JpaRepository<AgentHistory, UUID>{
 }
